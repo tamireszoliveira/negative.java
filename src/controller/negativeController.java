205f61489032ba5174563eac98f8108f27e77e49
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-13 15:30:21 -0300

    mudança enunciado

--- src/controller/negativeController.java
+++ src/controller/negativeController.java
@@ -2,11 +2,7 @@ package controller;
 
 public class negativeController{
 	
-	/*4. Crie uma função recursiva que exiba o total de elementos nega�vos de um vetor de inteiros, de N
-posições, passado como parâmetro:
-O código deve trazer como comentários:
-A condição de parada
-Como escrever a função para o termo n em função do termo anterior*/
+	
 	
 	public negativeController() {
 		super();
--- src/view/main.java
+++ src/view/main.java
@@ -3,6 +3,12 @@ package view;
 import controller.negativeController;
 
 public class main {
+	/*4. Crie uma função recursiva que exiba o total de elementos nega�vos de um vetor de inteiros, de N
+	posições, passado como parâmetro:
+	O código deve trazer como comentários:
+	A condição de parada
+	Como escrever a função para o termo n em função do termo anterior*/
+	
 	public static void main(String[] args) {
 		
 		int [] vetor = {56, -9, 6, -100, 0};
